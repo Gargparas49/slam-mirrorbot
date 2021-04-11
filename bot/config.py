76dd9dde27e22767e9a98002f80825a3384f2bdb
manikantaraavi <46994003+manikantaraavi@users.noreply.github.com>
2021-04-11 19:47:49 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,22 @@
+BOT_TOKEN: 1703714549:AAET4mPz5T8AaEraea54p7eQjxyq8c6K4FU
+GDRIVE_FOLDER_ID: This is the folder ID of the Google Drive Folder to which you want to upload all the mirrors.
+DOWNLOAD_DIR: The path to the local folder where the downloads should be downloaded to
+DOWNLOAD_STATUS_UPDATE_INTERVAL:10
+OWNER_ID:1001168252014
+AUTHORIZED_CHATS:1001168252014
+AUTO_DELETE_MESSAGE_DURATION: Interval of time (in seconds), 100
+IS_TEAM_DRIVE: (Optional field) Set to True if GDRIVE_FOLDER_ID is from a Team Drive else False or Leave it empty.
+USE_SERVICE_ACCOUNTS: (Optional field) (Leave empty if unsure) Whether to use service accounts or not. For this to work see "Using service accounts" section below.
+INDEX_URL: (Optional field) Refer to https://github.com/maple3142/GDIndex/ The URL should not have any trailing '/'
+API_KEY:2071376
+API_HASH:f5d7a035c84433157a11556ecb0f4cd6
+USE_TELEGRAPH:true
+MEGA_API_KEY: Mega.nz api key to mirror mega.nz links. Get it from Mega SDK Page
+MEGA_EMAIL_ID: Your email id you used to sign up on mega.nz for using premium accounts (Leave th)
+MEGA_PASSWORD: Your password for your mega.nz account
+STOP_DUPLICATE_MIRROR: (Optional field) (Leave empty if unsure) if this field is set to True , bot will check file in drive, if it is present in drive, downloading will ne stopped. (Note - File will be checked using filename, not using filehash, so this feature is not perfect yet)
+BLOCK_MEGA_FOLDER: (Optional field) If you want to remove mega.nz folder support, set it to True.
+BLOCK_MEGA_LINKS: (Optional field) If you want to remove mega.nz mirror support (bcoz it's too much buggy and unstable), set it to True.
+UPTOBOX_TOKEN: Uptobox token to mirror uptobox links. Get it from Uptobox Premium Account.
+SHORTENER_API: Fill your shortener api key if you are using shortener.
+SHORTENER: (Optional field) if you want to use shortener in Gdrive and index link, fill shotener url here. Examples:
